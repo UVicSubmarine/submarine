63bc5a49061cf0d36232df85a027b068c13be736
Liam Scott-Moncrieff <liam@w134-87-172-116.wireless.uvic.ca>
2019-03-17 17:23:26 -0700

    Added continuous conversion on ADC3 for direct control pot, added serial control of LAC.

--- src/main.c
+++ src/main.c
@@ -12,11 +12,22 @@
 
 #define VERSION "v0.1"
 #define DEBUG 1 //set debug mode, enabling serial communication
-#define LAC_PWM_TOP 125 //top value for PWM counter
+#define LAC_PWM_TOP 128 //top value for PWM counter
+
+//globals
+uint8_t serial_position_setpoint = 0;
 
 //control loop interrupt routine
 ISR(TIMER1_COMPA_vect){
    PORTB ^= (1 << 0); // Toggle the LED
+
+  if((PIND && PD7)){ // <<< TODO: fix so this properly evaluates PD7
+    OCR0B = (ADCH >> 1); //bit shift to reduce to 0-125
+  } else {
+    OCR0B = serial_position_setpoint;
+  }
+
+
 }
 
 //initialize control loop timer1
@@ -38,6 +49,14 @@ void initPWMTimer(void){
   OCR0B = 0; //set duty cycle = OCR0B/OCR0A * 100%
 }
 
+void initDirectControlPot(void){
+  DDRD |= (0 << PD7); //set up auto/manual control switch to input
+  ADMUX |= (1 << ADLAR) | (1 << REFS0)| (1 << MUX1) | (1 << MUX0); //left adjust, AVcc reference, pin PC3/ADC3
+  ADCSRA |= (1 << ADATE) | (1 << ADPS1) | (1 << ADPS0); //ADC prescale /8, free-run
+  ADCSRA |= (1 << ADEN); //Enable
+  ADCSRA |= (1 << ADSC); //start conversions
+}
+
 int main(void) {
 
   //set up serial communications for debug mode
@@ -46,18 +65,18 @@ int main(void) {
       printString("\n\nSubmarine Controller\n");
       printString(VERSION);
       printString("\nPress any key:\n");
-      while(!receiveByte());
+      receiveByte(); //wait to receive input
       printString("Starting Program\n");
   }
 
   // General initialization stuff
   initControlLoopTimer();
-  //initPWMTimer();
-
-  initMS5837();
+  initPWMTimer();
+  //initMS5837();
+  initDirectControlPot();
 
   while (1) {
-
+    serial_position_setpoint = getNumber()*1.28;
   }
   return 0;
 }
